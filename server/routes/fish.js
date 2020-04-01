8141e47b769f28236461c162145aa1cade2f482b
Meagan Pau <meagan.pau@crowdlinker.com>
2020-03-31 21:54:42 -0400

    hit my fish

--- server/routes/fish.js
+++ server/routes/fish.js
@@ -2,6 +2,7 @@ const FishSchema = require('../models/Fish');
 const fish = require('express').Router();
 
 fish.get('/', async (req, res, next) => {
+    console.log('hit my fish');
     try {
         await FishSchema.find({}, (err, data) => {
             res.status(200).json(data);
